1681d32e1b71ba60f65fb29cd039058b221ba82d
justina177 <justinaenenche@yahoo.com>
2023-10-31 12:39:46 +0100

    Added navlink items

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import { Container } from "reactstrap";
 
+const navLinks = [
+  {
+    display: "Home",
+    url: "#",
+  },
+  {
+    display: "About",
+    url: "#",
+  },
 
+  {
+    display: "Courses",
+    url: "#",
+  },
+  {
+    display: "Pages",
+    url: "#",
+  },
+  {
+    display: "Blog",
+    url: "#",
+  },
+];
 
 const Header = () => {
   return (
@@ -18,9 +40,16 @@ const Header = () => {
             <div className="nav__menu">
               <ul className="nal-list">
                 <li className="nav__item">
-                  <a href="home">Home</a>
+                  {navLinks.map((item, index) => (
+                    <li key={index} className="nav__item">
+                      <a href={item.url}>{item.display}</a>
+                    </li>
+                  ))}
                 </li>
               </ul>
+            </div>
+            <div className="nav__right">
+              <p><i className="ri-phone-line"></i>+234 87654321</p>
             </div>
           </div>
         </div>
